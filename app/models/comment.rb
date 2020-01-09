71fd5cc8fbbb6d24e56af7e76e48edf16e3163df
Tushar Adhao <tushar.adhao@icorprated.com>
2020-01-09 18:24:52 +0530

    Modification to add posts, comment and user functionality

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+
+	# Associations
+	belongs_to 	:parent, class_name: 'Comment', foreign_key: 'parent_id', :optional => true
+	has_many 	:children, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
+	belongs_to 	:post
+	belongs_to 	:user
+end
